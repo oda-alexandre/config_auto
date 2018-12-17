6b6fb00a29fd05bb211da10cc2e2e9f960d7f876
oda-alexandre <oda.alexandre31@gmail.com>
2018-12-17 12:04:06 +0100

    ajout commentaires

--- anonymat/hostnamerandom.py
+++ anonymat/hostnamerandom.py
@@ -6,12 +6,14 @@ module pour l'installation d'un script de hostname aléatoire automatique
 
 print("\033[36;1m \nInstallation des prerequis\n \033[0m")
 
+# INSTALLATION DES PREREQUIS
 os.system("sudo apt-get update && \
 sudo apt install --no-install-recommends -y \
 git")
 
 print("\033[36;1m \nMise en place du script\n \033[0m")
 
+# CREATION DU DOSSIER /usr/share/wordlists & INSTALLATION DU SCRIPT /etc/init.d/hostname-random
 os.system("sudo mkdir /usr/share/wordlists && \
 git clone https://github.com/oda-alexandre/hostname_random.git && \
 sudo mv -f hostname_random/names.txt /usr/share/wordlist/ && \
