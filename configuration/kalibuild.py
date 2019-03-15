74abfd5689e29d4ed59f4a433000b63b4a5ded16
oda-alexandre <oda.alexandre31@gmail.com>
2019-03-15 20:54:30 +0000

    update

--- configuration/kalibuild.py
+++ configuration/kalibuild.py
@@ -21,8 +21,8 @@ os.system("git clone git://git.kali.org/live-build-config.git && \
 cd live-build-config/kali-config/ && \
 find . -maxdepth 1 ! -name . ! -name 'variant-default' -exec rm -rf {} \; && \
 git clone https://github.com/oda-alexandre/kali_build.git && \
-mv kali_build/common . && \
-mv kali_build/variant-gnome . && \
+mv kali_build/common ../ && \
+mv kali_build/variant-gnome ../ && \
 rm -rf kali_build/ && \
 cd ../")
 
