a9b39d38dd394b9c607cd291ca6835a661635a19
oda-alexandre <oda.alexandre31@gmail.com>
2019-04-17 20:19:28 +0000

    debug theme sombre

--- configuration/themesombre.py
+++ configuration/themesombre.py
@@ -9,5 +9,7 @@ import os
 # CONFIGURATION DU FICHIER .config/gtk-3.0/settings.ini AVEC dark-theme
 print("\033[36;1m \nConfiguration du fichier .config/gtk-3.0/settings.ini\n \033[0m")
 
-os.system("echo '[Settings]' >> $HOME/.config/gtk-3.0/settings.ini && \
+os.system("mkdir -p $HOME/.config/gtk-3.0 && \
+touch $HOME/.config/gtk-3.0/settings.ini && \
+echo '[Settings]' >> $HOME/.config/gtk-3.0/settings.ini && \
 echo 'gtk-application-prefer-dark-theme=1' >> $HOME/.config/gtk-3.0/settings.ini")
